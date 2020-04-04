a3bd2fb24b075b9ccd76e4fd7bd10574c6cdfc3a
Yash Mangla <yash.mangla378@gmail.com>
2020-04-04 19:21:02 +0530

    Express setup with necesary dependencies

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
