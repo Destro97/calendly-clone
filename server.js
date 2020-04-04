64dcd6f78e5b00bc29fb90e7cae516eac9b5a529
Yash Mangla <yash.mangla378@gmail.com>
2020-04-04 20:10:12 +0530

    added route as middleware in server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json({ extended: false }));
 
+app.use("/api", require("./routes"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
